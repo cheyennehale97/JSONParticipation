60cb5de3fdde295bf218f80dbd479ade800fe6e8
Cheyenne Hale <Cheyenne.C.Hale-1@ou.edu>
2019-02-25 11:47:45 -0600

    WIP

--- GOTQuotes/MainWindow.xaml.cs
+++ GOTQuotes/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿//Cheyenne Hale
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Console;
 
 namespace GOTQuotes
 {
@@ -23,6 +27,30 @@ namespace GOTQuotes
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+
+            using (HttpClient client = new HttpClient())
+            {
+                var response = client.GetAsync(@"https://got-quotes.herokuapp.com/quotes").Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = response.Content.ReadAsStringAsync().Result;
+                    HerokuQuote q = JsonConvert.DeserializeObject<HerokuQuote>(content);
+                    quote.Inlines.Add(new Italic());
+                    quote.Inlines.Add("-");
+                    quote.Inlines.Add(new Bold());
+                    
+                    
+
+                    
+                }
+            }
+
+
         }
     }
 }
